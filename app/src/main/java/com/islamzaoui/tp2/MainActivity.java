51682efb682fe095a8fb4f5104cb1c585502df45
Islam Zaoui <zaoui.islam2021@gmail.com>
2024-04-27 15:20:59 +0100

    add menu

---
+++ app/src/main/java/com/islamzaoui/tp2/MainActivity.java
@@ -0,0 +1,41 @@
+package com.islamzaoui.tp2;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId() == R.id.memo) {
+
+        }
+        else if(item.getItemId() == R.id.conversion) {
+
+        }
+        else {
+            return super.onOptionsItemSelected(item);
+        }
+        return true;
+    }
+}
