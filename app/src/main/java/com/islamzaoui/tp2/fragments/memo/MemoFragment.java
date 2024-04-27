3aae27ebc1a3b503d3d2e72884dcf2febd345812
Islam Zaoui <zaoui.islam2021@gmail.com>
2024-04-27 15:45:05 +0100

    setup memo and conversion for quantity fragments

--- app/src/main/java/com/islamzaoui/tp2/fragments/memo/MemoFragment.java
+++ app/src/main/java/com/islamzaoui/tp2/fragments/memo/MemoFragment.java
@@ -2,7 +2,10 @@ package com.islamzaoui.tp2.fragments.memo;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+import androidx.viewpager2.widget.ViewPager2;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,57 +13,40 @@ import android.view.ViewGroup;
 
 import com.islamzaoui.tp2.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link MemoFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.ArrayList;
+import java.util.List;
+
 public class MemoFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public MemoFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment MemoFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static MemoFragment newInstance(String param1, String param2) {
-        MemoFragment fragment = new MemoFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_memo, container, false);
+
+        ViewPager2 quantityViewPager = view.findViewById(R.id.quantityViewPager);
+        QuantityPagerAdapter quantityPagerAdapter = new QuantityPagerAdapter(this);
+        quantityViewPager.setAdapter(quantityPagerAdapter);
+
+        return view;
+    }
+
+    private static class QuantityPagerAdapter extends FragmentStateAdapter {
+        private final List<Fragment> fragments = new ArrayList<>();
+        public QuantityPagerAdapter(@NonNull Fragment fragment) {
+            super(fragment);
         }
-    }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_memo, container, false);
+        @NonNull
+        @Override
+        public Fragment createFragment(int position) {
+            if(fragments.isEmpty())
+                return null;
+
+            return fragments.get(position);
+        }
+
+        @Override
+        public int getItemCount() {
+            return fragments.size();
+        }
     }
 }
